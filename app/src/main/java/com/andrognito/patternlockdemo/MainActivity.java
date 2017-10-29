be03f47076c6012ca5bb538c2e10ec400c58342f
hk0792 <hk0792>
2017-10-29 17:41:00 +0800

    rebuild

--- app/src/main/java/com/andrognito/patternlockdemo/MainActivity.java
+++ app/src/main/java/com/andrognito/patternlockdemo/MainActivity.java
@@ -14,6 +14,7 @@ import com.andrognito.rxpatternlockview.RxPatternLockView;
 import com.andrognito.rxpatternlockview.events.PatternLockCompleteEvent;
 import com.andrognito.rxpatternlockview.events.PatternLockCompoundEvent;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import io.reactivex.functions.Consumer;
@@ -55,26 +56,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mPatternLockView = (PatternLockView) findViewById(R.id.patter_lock_view);
-        mPatternLockView.setDotCount(3);
-        mPatternLockView.setDotNormalSize((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_dot_size));
-        mPatternLockView.setDotSelectedSize((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_dot_selected_size));
-        mPatternLockView.setPathWidth((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_path_width));
-        mPatternLockView.setAspectRatioEnabled(true);
-        mPatternLockView.setAspectRatio(PatternLockView.AspectRatio.ASPECT_RATIO_HEIGHT_BIAS);
-        mPatternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT);
-        mPatternLockView.setDotAnimationDuration(150);
-        mPatternLockView.setPathEndAnimationDuration(100);
-        mPatternLockView.setCorrectStateColor(ResourceUtils.getColor(this, R.color.white));
+//        mPatternLockView.setDotCount(3);
+//        mPatternLockView.setDotNormalSize((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_dot_size));
+//        mPatternLockView.setDotSelectedSize((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_dot_selected_size));
+//        mPatternLockView.setPathWidth((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_path_width));
+//        mPatternLockView.setAspectRatioEnabled(true);
+//        mPatternLockView.setAspectRatio(PatternLockView.AspectRatio.ASPECT_RATIO_HEIGHT_BIAS);
+        mPatternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
+        ArrayList<PatternLockView.Dot> list=new ArrayList<>();
+        list.add(PatternLockView.Dot.of(5));
+        list.add(PatternLockView.Dot.of(6));
+//        mPatternLockView.setPattern(PatternLockView.PatternViewMode.AUTO_DRAW, list);
+//        mPatternLockView.setDotAnimationDuration(150);
+//        mPatternLockView.setPathEndAnimationDuration(100);
+//        mPatternLockView.setCorrectStateColor(ResourceUtils.getColor(this, R.color.white));
         mPatternLockView.setInStealthMode(false);
         mPatternLockView.setTactileFeedbackEnabled(true);
         mPatternLockView.setInputEnabled(true);
-        mPatternLockView.addPatternLockListener(mPatternLockViewListener);
+//        mPatternLockView.addPatternLockListener(mPatternLockViewListener);
+        ;
+
 
         RxPatternLockView.patternComplete(mPatternLockView)
                 .subscribe(new Consumer<PatternLockCompleteEvent>() {
                     @Override
                     public void accept(PatternLockCompleteEvent patternLockCompleteEvent) throws Exception {
                         Log.d(getClass().getName(), "Complete: " + patternLockCompleteEvent.getPattern().toString());
+                        mPatternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
                     }
                 });
 
